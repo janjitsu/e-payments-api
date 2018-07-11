bb8e93a1e4cfac7f69c5141d7e86b27e283d102e
Jan Silva <janfrs3@gmail.com>
2018-07-10 23:11:09 -0300

    Add test

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\DefaultController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+}
