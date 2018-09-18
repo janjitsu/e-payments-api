81dca4f0527e143c756d1fdf01ccc651be78d5ea
Jan Silva <janfrs3@gmail.com>
2018-09-17 23:53:49 -0300

    WIP: Prepare fields for credit card submit

--- src/Controller/PagSeguroController.php
+++ src/Controller/PagSeguroController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use PagSeguro\Library as PagSeguroLibrary;
 use PagSeguro\Services\Session as PagSeguroSession;
+use PagSeguro\Services\Installment as PagSeguroInstallment;
 use PagSeguro\Configuration\Configure as PagSeguroConfigure;
 use PagSeguro\Domains\Requests\DirectPayment\CreditCard as PagSeguroCreditCard;
 
@@ -29,7 +30,9 @@ class PagSeguroController extends Controller
                 PagSeguroConfigure::getAccountCredentials()
             );
 
-            return new JsonResponse(['sessionId' => $sessionCode->getResult()]);
+            return new JsonResponse([
+                'sessionId' => $sessionCode->getResult(),
+            ]);
         } catch (Exception $e) {
             return new JsonResponse(['error' => $e->getMessage()],400);
         }
@@ -40,6 +43,26 @@ class PagSeguroController extends Controller
      **/
     public function postCreditCardPayment(Request $request)
     {
+        //parameters
+        $creditCardToken    = $request->get('creditCardToken');
+        $senderHash         = $request->get('senderHash');
+        $receiverEmail      = $request->get('receiverEmail');
+        $donationAmount     = $request->get('donationAmount');
+
+        //billing
+        $street             = $request->get('street');
+        $number             = $request->get('number');
+        $neighborhood       = $request->get('neighborhood');
+        $zipCode            = $request->get('zipCode');
+        $city               = $request->get('city');
+        $state              = $request->get('state');
+        $complement         = $request->get('complement');
+
+        $birthDate          = $request->get('birthDate');
+        $buyerName          = $request->get('buyerName');
+        $buyerCPF           = $request->get('buyerCPF');
+        $buyerPhone         = $request->get('buyerPhone');
+
         //gather fields and post to pagseguro
         //Instantiate a new direct payment request, using Credit Card
         $creditCard = new PagSeguroCreditCard();
@@ -47,7 +70,7 @@ class PagSeguroController extends Controller
         /**
          * @todo Change the receiver Email
          */
-        $creditCard->setReceiverEmail('janfrs3@gmail.com');
+        $creditCard->setReceiverEmail($receiverEmail);
 
         // Set a reference code for this payment request. It is useful to identify this payment
         // in future notifications.
@@ -60,13 +83,13 @@ class PagSeguroController extends Controller
         $creditCard->addItems()->withParameters(
             '0001',
             'Donation',
-            2,
-            30.00
+            1,
+            $donationAmount
         );
 
         // Set your customer information.
         // If you using SANDBOX you must use an email @sandbox.pagseguro.com.br
-        $creditCard->setSender()->setName('João Comprador');
+        $creditCard->setSender()->setName('Equale');
         $creditCard->setSender()->setEmail('comprador@sandbox.pagseguro.com.br');
         $creditCard->setSender()->setPhone()->withParameters(
             11,
@@ -78,7 +101,7 @@ class PagSeguroController extends Controller
             '06361814483'
         );
 
-        $creditCard->setSender()->setHash($request->get('senderHash'));
+        $creditCard->setSender()->setHash($senderHash);
 
         $creditCard->setSender()->setIp('127.0.0.0');
 
@@ -107,11 +130,11 @@ class PagSeguroController extends Controller
         );
 
         // Set credit card token
-        $creditCard->setToken($request->get('creditCardToken'));
+        $creditCard->setToken($creditCardToken);
 
         // Set the installment quantity and value (could be obtained using the Installments
         // service, that have an example here in \public\getInstallments.php)
-        $creditCard->setInstallment()->withParameters(1, '30.00');
+        $creditCard->setInstallment()->withParameters(1, $donationAmount);
 
         // Set the credit card holder information
         $creditCard->setHolder()->setBirthdate('01/10/1979');
